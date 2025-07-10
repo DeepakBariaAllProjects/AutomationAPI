2e5cf9a35a528e2812d7610da34fac8104680365
DeepakBariaAllProjects <bariyadpk1999@gmail.com>
2025-07-10 15:52:54 +0530

    validation API

--- AutomationApi/src/test/java/Session04/CheckForValidResponse.java
+++ AutomationApi/src/test/java/Session04/CheckForValidResponse.java
@@ -1,11 +1,30 @@
 package Session04;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
+
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
 
 class CheckForValidResponse {
 
 	@Test
 	public void GetsingleUser() {
+		//Specify base URL
+		RestAssured.baseURI= "https://reqres.in/api/users/2";
+		
+		//Get Request specification of the request
+		RequestSpecification requestSpec  = RestAssured.given();
+		
+		//call get  method 
+		 Response response = requestSpec.get();
+		 
+		 //gets response code 
+		 int statusCode  = response.getStatusCode();
+		 
+		 //validate actual status code with expected
+		 Assert.assertEquals(statusCode, 200);
 		
 	}
 }
